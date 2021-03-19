8f92ce63f3f29f99efa3d5d85736a752a64ad947
cisu <49948070+cisu@users.noreply.github.com>
2021-03-19 20:21:10 +0200

    User Authentication/Login Route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+// https://express-validator.github.io/docs/
+// const {check, validationResult} = require('express-validator/check');
+const {check, validationResult} = require('express-validator');
 
 const User = require('../../models/Users');
 
@@ -20,6 +26,76 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @router  POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+
+  // Checking for errors in the body
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+
+    // destructuring req.body
+    const {email, password} = req.body;
+
+    // Make a query with mongoose
+    try {
+      // See if user exists already
+      let user = await User.findOne({email});
+
+      // check if not a user
+      if (!user) {
+        return res.status(400).json({
+          errors: [{msg: 'Invalid Credentials'}],
+        });
+      }
+
+      // Check if the password matches
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({
+          errors: [{msg: 'Invalid Credentials'}],
+        });
+      }
+
+      // Return jsonwebtoken
+      // get the payload which includes the user id
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // sign the token
+      jwt.sign(
+        // pass the payload
+        payload,
+        // pass the secret
+        config.get('jwtSecret'),
+        // expiration
+        {expiresIn: 3600000},
+        // inside the callback we'll get either an error or a token
+        (err, token) => {
+          if (err) throw err;
+          res.json({token});
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 // export the route
 module.exports = router;
